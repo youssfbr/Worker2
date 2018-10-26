4f0f6cb58903e786a140210a3744435adb956ea9
Alisson Youssf <youssfbr@gmail.com>
2018-10-26 14:21:47 -0300

    Classe Worker e relacionamentos

--- src/entities/Worker.java
+++ src/entities/Worker.java
@@ -1,5 +1,8 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import entities.enums.WorkerLevel;
 
 public class Worker {
@@ -7,4 +10,7 @@ public class Worker {
 	private String name;
 	private WorkerLevel level;
 	private Double baseSalary;
+	
+	private Department department;
+	private List<HourContract> contracts = new ArrayList<>();
 }
